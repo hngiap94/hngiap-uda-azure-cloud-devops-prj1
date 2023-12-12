a6d8643ed6651560c1aa6dead05cb59a0548f638
hngiap94 <hngiap94@gmail.com>
2023-12-12 14:35:16 +0700

    add network security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,9 @@ provider "azurerm" {
 data "azurerm_image" "app" {
   name                = "udacity-packer-image"
   resource_group_name = var.resource_group
-  location = var.location
 }
 
+# Virtual network and subnet
 resource "azurerm_virtual_network" "main" {
   name                = "${var.project}-network"
   address_space       = ["10.0.0.0/16"]
@@ -20,6 +20,52 @@ resource "azurerm_virtual_network" "main" {
   }
 
   tags = {
-    project     = "udacity1"
+    project     = var.project
+  }
+}
+
+# Network security group
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.project}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  security_rule {
+    name                       = "AllowInboundInternal"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+  security_rule {
+    name                       = "AllowOutboundInternal"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "DenyDirectAccessFromtheInternet"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  tags = {
+    project = var.project
   }
 }
